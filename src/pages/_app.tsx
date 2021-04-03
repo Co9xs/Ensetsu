c33798e999cb241629e0ede21248be422af23ce3
Co9xs <ryo.fjsm0203@icloud.com>
2021-04-03 12:38:50 +0900

    NProgress loading indicatorを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,17 @@
 import 'modern-css-reset/dist/reset.min.css'
 import React from 'react';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css'
 import { AppProps } from 'next/app';
 import { AuthProvider } from '../context/Auth';
 import { HeaderLayout } from '../components/layouts/HeaderLayout';
 import { NoHeaderLayout } from '../components/layouts/NoHeaderLayout';
+import { Router } from 'next/router';
+
+NProgress.configure({ showSpinner: false })
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   switch (pageProps.layout) {
